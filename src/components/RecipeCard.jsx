6dcf55ccef8c023576dd73b1a2a6fad3a60d3ade
Ahmat Prayoga Sembiring <ahmadprayoga099@gmail.com>
2025-11-03 11:08:26 +0700

    ubah

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -6,13 +6,9 @@ function RecipeCard({ recipe, onSelect, onToggleFavorite, isFavorited }) {
   return (
     <div className="recipe-card">
       <div className="recipe-image-container">
-        <img src={recipe.strMealThumb || "/placeholder.svg"} alt={recipe.strMeal} className="recipe-image" />
-        <button
-          className={`favorite-btn ${isFavorited ? "favorited" : ""}`}
-          onClick={() => onToggleFavorite(recipe)}
-          title={isFavorited ? "Remove from favorites" : "Add to favorites"}
-        >
-          ♥
+        <img src={recipe.strMealThumb || '/placeholder.svg'} alt={recipe.strMeal} className="recipe-image" />
+        <button className={`favorite-btn ${isFavorited ? 'favorited' : ''}`} onClick={() => onToggleFavorite(recipe)} title={isFavorited ? 'Remove from favorites' : 'Add to favorites'}>
+          ❤️
         </button>
       </div>
       <div className="recipe-info">
@@ -26,7 +22,7 @@ function RecipeCard({ recipe, onSelect, onToggleFavorite, isFavorited }) {
         </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default RecipeCard
